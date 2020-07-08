63950698b5b3f77719f40a3bc30cfc83c88adcaa
haotingfang <haotingfang@foxmail.com>
2020-07-08 17:24:13 +0800

    '---------------------------'

--- src/main/java/com/example/redislockdemo/MyRedisLock1.java
+++ src/main/java/com/example/redislockdemo/MyRedisLock1.java
@@ -1,8 +1,8 @@
 package com.example.redislockdemo;
 
-public class MyRedisLock {
+public class MyRedisLock1 {
 
     public static void main(String[] args) {
-        System.out.println("MyRedisLock");
+        System.out.println("MyRedisLock1");
     }
 }
