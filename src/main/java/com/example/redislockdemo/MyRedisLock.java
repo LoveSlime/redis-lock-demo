089f7bea36c2c05c4f008c2610985b0ce255f4c2
haotingfang <haotingfang@foxmail.com>
2020-07-08 11:03:03 +0800

    idea测试 add commit

--- src/main/java/com/example/redislockdemo/MyRedisLock.java
+++ src/main/java/com/example/redislockdemo/MyRedisLock.java
@@ -2,5 +2,7 @@ package com.example.redislockdemo;
 
 public class MyRedisLock {
 
-
+    public static void main(String[] args) {
+        System.out.println("11111");
+    }
 }
--- src/main/java/com/example/redislockdemo/RedisLockDemoApplication.java
+++ src/main/java/com/example/redislockdemo/RedisLockDemoApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class RedisLockDemoApplication {
-
+    /*添加备注*/
     public static void main(String[] args) {
         SpringApplication.run(RedisLockDemoApplication.class, args);
     }
