37b78720defc15213ca99a71670df135008290c4
haotingfang <haotingfang@foxmail.com>
2020-07-08 17:53:08 +0800

    '---MyRedisLock1738'

--- src/main/java/com/example/redislockdemo/MyRedisLock.java
+++ src/main/java/com/example/redislockdemo/MyRedisLock.java
@@ -5,5 +5,6 @@ public class MyRedisLock {
     public static void main(String[] args) {
         System.out.println("MyRedisLock");
         System.out.println("MyRedisLock1730");
+        System.out.println("MyRedisLock1738");
     }
 }
